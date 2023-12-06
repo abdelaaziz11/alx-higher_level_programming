5f4d9ea07efe0b61b588f8de82d4708b02d96cf9
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-12-06 14:56:55 -0800

    roman to in

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    val = {'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    b = 0
+    if  type(roman_string) != str or roman_string == None:
+        return 0
+    else:
+        for i in range(len(roman_string) - 1, -1, -1):
+            if val[roman_string[i]] >= b:
+                res += val[roman_string[i]]
+            else:
+                res -= val[roman_string[i]]
+            b = val[roman_string[i]]
+    return res
