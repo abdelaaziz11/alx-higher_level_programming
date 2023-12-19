87528e2ade14aa80f20cb714e1635f0397de8de3
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-12-19 04:30:49 -0800

    Emty class Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-""" Write an empty class Square """
+"""Write an empty class Square"""
 
 
 class Square:
+    """This class defines properties of an empty square.
+    """
     pass
