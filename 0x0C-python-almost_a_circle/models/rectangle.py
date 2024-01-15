d9b5d87dc0ca7f772133a1849810448ff933368b
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-01-15 07:19:36 -0800

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module Rectangle class"""
+
+
+class Rectangle:
+    """A representation of the Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init def"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """with of rectangle"""
+        return self.__width
+
+    @width.setter
+    def __width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        """hieght of rectangle"""
+        return self.__height
+
+    @height.setter
+    def __height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """x of rectangle"""
+        return self.__x
+
+    @x.setter
+    def __x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """y of rectangle"""
+        return self.__y
+
+    @y.setter
+    def __y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
