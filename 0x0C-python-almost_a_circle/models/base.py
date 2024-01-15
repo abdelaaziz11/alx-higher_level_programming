8b665d47c60c910f863317ad12aa84f7022733ab
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-01-15 07:10:30 -0800

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module bases class"""
+
+
+class Base:
+    """A representation of the base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Constractor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
