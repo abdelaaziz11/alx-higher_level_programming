0e2c3b4ddf99e921c050c3ed2633c5d291a20b69
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-13 11:28:09 -0800

    ano link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
 -- ists all records of the table second_table of
 --the database hbtn_0c_0 in your MySQL server.
 
-SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table
+    WHERE name IS NOT NULL
+    ORDER BY score DESC;
