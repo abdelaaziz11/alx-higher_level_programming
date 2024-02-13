59d3aa72e0bca4edcf3c065299968995d8ee46e3
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-13 11:32:35 -0800

    ano link

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
---  inserts a new row in the table first_table database hbtn_0c_0
-
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+--  inserts a new row in the table first_table database
+INSERT INTO first_table VALUES (89, 'Best School');
