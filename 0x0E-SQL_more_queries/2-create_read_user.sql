36d6242a4ca010647d255f13e2d6a3921f5b4025
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-14 09:30:29 -0800

    create user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 
-CREATE DATABASE
+/*CREATE DATABASE
 	IF NOT EXISTS `hbtn_0d_2`
 CREATE USER
 	IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2'
@@ -8,4 +8,12 @@ CREATE USER
 GRANT SELECT
 	ON `hbtn_0d_2`.*
 	TO 'user_0d_2'@'hbtn_0d_2'
-	IDENTIFIED BY 'user_0d_2_pwd';
+	IDENTIFIED BY 'user_0d_2_pwd';*/
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to the user on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
