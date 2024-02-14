4a33dcc9a49f981a1cb4d8b3aebac7afe5e372da
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-14 12:48:44 -0800

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,10 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT id, name FROM cities
+SELECT id, name
+FROM cities
 WHERE sataes_id = (
 	SELECT id
 	FROM states
-	WHERE name='California'
+	WHERE name = 'California'
 )
 ORDER BY id ASC;
