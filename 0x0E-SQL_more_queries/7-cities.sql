c94d7fd78fe779916d788974a6589b888701d037
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-14 11:24:33 -0800

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
 	id INT AUTO_CREMENT PRIMARY KEY,
-	states_id INT NOT NULL FOREIGN KEY LIKE id OF states,
-	name VARCHAR(256) NOT NULL
+	states_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (states_id) REFERENCES states(id)
 );
