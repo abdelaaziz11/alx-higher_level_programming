6168b3f95acc81ec1c527da00505d75b7ee0e0ad
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-14 11:06:45 -0800

    states table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,8 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'states'@'%'
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states
 (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
