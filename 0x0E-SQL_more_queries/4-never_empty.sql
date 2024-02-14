9e780f9c227b26a8af26a3525b971d8dba99bdb3
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-02-14 09:47:48 -0800

    create table never empty

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,6 @@
 -- creates the table id_not_null on your MySQL server.
-
+SELECT 1 FROM information_schema.tables
+WHERE table_schema = DATABASE() AND table_name = 'id_not_null' LIMIT 1;
 CREATE TABLE IF NOT EXISTS id_not_null
 (
 	id INT = DEFAULT 1,
