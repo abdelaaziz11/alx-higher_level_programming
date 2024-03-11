4a23554c673db60ba3b93fb9316dcf7216815bfe
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-11 08:55:02 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+// checks if the arguments found
+
 const count = process.argv.length;
-if (count === 2)
-	console.log('No argument');
-else if (count === 3)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
