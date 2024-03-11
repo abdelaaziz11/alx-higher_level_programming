dd11d7b679782d30f6c4c6c4aa79248717767b8e
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-11 10:15:42 -0700

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,16 +3,13 @@
 if (process.argv[2] === undefined) {
   if (process.argv[3] === undefined) {
     console.log('undefined' + ' is ' + 'undefined');
-  }
-  else {
+  } else {
     console.log('undefined' + ' is ' + process.argv[3]);
   }
 } else {
   if (process.argv[3] === undefined) {
     console.log(process.argv[2] + ' is ' + 'undefined');
-    }
-    else {
+  } else {
     console.log(process.argv[2] + ' is ' + process.argv[3]);
-    }
+  }
 }
-
