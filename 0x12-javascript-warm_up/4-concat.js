bf98d522ede5bccad78acdde4bf303e850103244
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-11 10:11:14 -0700

    create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined) {
+  if (process.argv[3] === undefined) {
+    console.log('undefined' + ' is ' + 'undefined');
+  }
+  else {
+    console.log('undefined' + ' is ' + process.argv[3]);
+  }
+} else {
+  if (process.argv[3] === undefined) {
+    console.log(process.argv[2] + ' is ' + 'undefined');
+    }
+    else {
+    console.log(process.argv[2] + ' is ' + process.argv[3]);
+    }
+}
+
