6958abf86b95f59559915bd57bfd2262ed521294
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-11 17:16:09 -0700

    second biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// searches the second biggest integer
+
+if (process.argv.lenght <= 3) {
+  console.log(0);
+} else {
+  const s = process.argv.sort((a, b) => b - a);
+  console.log(s.slice(3)[0]);
+}
