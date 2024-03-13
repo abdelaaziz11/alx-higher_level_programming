cfe065a811bea19f6ec5ee398f855008ece29972
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-12 18:26:06 -0700

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
-const x = fs.readFileSync(process.argv[2], 'utf8');
-const y = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], x + y);
+// The first argument is the file path of the first source file
+// The second argument is the file path of the second source file
+// The third argument is the file path of the destination
+
+const s = require('s');
+const x = s.readFileSync(process.argv[2], 'utf8');
+const y = s.readFileSync(process.argv[3], 'utf8');
+s.writeFileSync(process.argv[4], x + y);
