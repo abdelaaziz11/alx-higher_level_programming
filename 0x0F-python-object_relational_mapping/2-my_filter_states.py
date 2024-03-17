2d8a7749ab7af1b319d5916932e3bad825ec2b74
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-16 18:50:37 -0700

    Filter states by use input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
 
     cursor = connection.cursor()
 
-    cursor.execute(" SELECT * FROM states WHERE BINARY name = '{}' ".format(argv[4]))
+    cursor.execute(" SELECT * FROM states\
+            WHERE BINARY name = '{}' ".format(argv[4]))
 
     results = cursor.fetchall()
 
