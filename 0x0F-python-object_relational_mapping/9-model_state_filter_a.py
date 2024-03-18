68572a99cd43616fd846a68470c8abdafaacae79
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-17 18:37:07 -0700

    model states filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-lists all State objects from the database hbtn_0e_6_usa
+lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa
 """
 
 from model_state import Base, State
