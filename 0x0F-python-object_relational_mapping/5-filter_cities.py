68f374ee8042b49b23c4f0fdca2a6ed5c7f3d20a
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-17 17:07:58 -0700

    Filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-'''
-takes in the name of a state as an argument and lists all cities of that state,
-using the database hbtn_0e_4_usa
-'''
+"""script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa"""
 
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-
     connection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
             user=argv[1],
             passwd=argv[2],
-            db=argv[3]
-    )
+            db=argv[3],
+            port=3306
+            )
 
     cursor = connection.cursor()
- 
-    query = "SELECT cities.id, cities.name, states.name\
-        FROM cities\
-        JOIN states ON cities.state_id = states.id\
-        WHERE states.name = %s\
-        ORDER BY cities.id ASC"
-    """ SELECT cities.name\
-            FROM JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s """
-    cursor.execute(query, (argv[4],))
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
 
-    cursor.close()
-    connection.close()
+    cursor.execute("SELECT cities.name\
+            FROM cities\
+            JOIN states ON cities.state_id = states.id\
+            WHERE states.name = '{}'".format(argv[4]))
+
+    results = cursor.fetchall()
+
+    """cities = [row[0] for row in results]
+    print(", ".join(cities))"""
+
+    cities = list(map(lambda row: row[0], results))
+    print(", ".join(cities))
+
