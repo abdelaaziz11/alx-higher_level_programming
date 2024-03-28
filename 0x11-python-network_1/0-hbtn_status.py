289020c2cf614802be6615f0c8d7db721f647db5
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-28 03:16:49 -0700

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 '''
+fetches https://alx-intranet.hbtn.io/status
 '''
-import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+
+if __name__ == "__main__":
+    import urllib.request
+
+    req = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode('utf8')))
