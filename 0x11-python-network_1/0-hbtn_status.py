171d14fa81e5ce44f9c70bf5c56b2967e61589c7
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-28 03:20:21 -0700

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,8 @@ fetches https://alx-intranet.hbtn.io/status
 if __name__ == "__main__":
     import urllib.request
 
-    req = urllib.request
-    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-
-        the_page = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        the_page = resp.read()
         print("\t- type: {}".format(type(the_page)))
         print("\t- content: {}".format(the_page))
         print("\t- utf8 content: {}".format(the_page.decode('utf8')))
