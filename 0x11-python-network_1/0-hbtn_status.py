3f626eb78c95980f1eaace156872cef3c72db50f
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-28 03:01:31 -0700

    my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+'''
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
