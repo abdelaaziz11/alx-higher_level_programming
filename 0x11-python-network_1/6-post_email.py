a855876237375361aaf0ef95a8a9a595f8b4cb73
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-29 16:49:28 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''  POST request to the passed URL with the email as a parameter '''
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    req = requests.post(url, data=value)
+    print(req.text)
