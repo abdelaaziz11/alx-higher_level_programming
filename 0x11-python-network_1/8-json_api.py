46853c8796b595f4c74bfe49ec0f118aa1e5173e
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-03-29 17:02:44 -0700

    JSON API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,10 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    if len(sys.argv) == 1
+        letter = ""
+    else:
+        sys.argv[1]
     payload = {"q": letter}
 
     req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
