a39e735ffb2b2532dad8fdccfb0b4be0e40d011e
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-23 09:26:49 -0700

    Read and print content of a file by fs module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,15 +5,14 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-	console.error("Erorr");
-	process.exit(1);
+  console.error('Erorr');
+  process.exit(1);
 }
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
-
