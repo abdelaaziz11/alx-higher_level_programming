332d86e61833323b2f3d9177ede2ad527bdc0544
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-23 14:26:17 -0700

    Print all characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,9 +4,19 @@ const request = require('request');
 const movieID = process.argv[2];
 const urlFilms = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request.get(urlFilms, (err, data) => {
+request.get(urlFilms, (err, response, body) => {
   if (err) {
     console.error(err);
   }
-  console.log(data);
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (character of characters) {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.error(err);
+      }
+      const charData = JSON.parse(body);
+      console.log(charData.name);
+    });
+  }
 });
